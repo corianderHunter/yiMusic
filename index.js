e6178250cb5b810018e3e9262c2a9b573a035b1c
donghua.wei <donghua.wei@sky-data.cn>
2017-08-03 17:19:06 +0800

    测试mysql连接

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 /**
  * Created by wdh on 2017/8/3.
  */
-const address = 'localhost:'
+var mysql = require('mysql');
+var bluebird = require('bluebird');
+
+const host = 'localhost';
+const port = 3306;
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host,
+    user            : 'root',
+    password        : 'root',
+    database        : 'yiMusic'
+});
+
+var getConnect = bluebird.promisify(pool.getConnection);
+
+module.exports = getConnect;
---
+++ test/testDBCon.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wdh on 2017/8/3.
+ */
+var getCon = require('../config/DBCONFIG')
+
+getCon().then(function(con){
+            console.log('连接成功！')
+            console.log(con);
+        })
+        .catch(err=>{
+            console.log('连接数据发生错误：'+err);
+        })
